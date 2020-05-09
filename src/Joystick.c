0906ecd948dec4d5ece29b2ec5242523127d02cb
Anthony Sottile <asottile@umich.edu>
2020-05-09 12:34:22 -0700

    un-plugin-ize the code

--- src/Joystick.c
+++ src/Joystick.c
@@ -1,6 +1,58 @@
 #include "Joystick.h"
 #include "Bot.h"
 
+const int ECHOES = 0;
+
+const __flash command INPUTS[] = {
+    // Setup controller
+                          { NOTHING,  250 },
+    { TRIGGERS,   5 },    { NOTHING,  150 },
+    { TRIGGERS,   5 },    { NOTHING,  150 },
+    { A,          5 },    { NOTHING,  250 },
+
+    // Go into game
+    { HOME,       5 },    { NOTHING,  250 },
+    { A,          5 },    { NOTHING,  250 },
+
+    // spam A
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 },
+    { A,          5 },    { NOTHING,  5 }
+};
+
+const int INPUT_REPEAT_BEGIN = 11;
+const int INPUTS_LENGTH = sizeof(INPUTS)/sizeof(command);
+
 // Main entry point.
 int main(void) {
     // We'll start by performing hardware and peripheral setup.
