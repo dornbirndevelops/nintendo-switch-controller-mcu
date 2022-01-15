9011fc3ce45d7e12c88fa2588a4e6afd24bb6875
dornbirndevelops <dornbirndevelops@gmail.com>
2022-01-15 23:48:12 +0100

    added routes info, begin implementation

--- apps/hello_world/src/main.c
+++ apps/hello_world/src/main.c
@@ -7,7 +7,24 @@
 #include <zephyr.h>
 #include <sys/printk.h>
 
+/* routes:
+* / -> serve web frontend
+* /api/connect -> connect to switch console
+* /api/disconnect -> disconnect from switch console
+* /api/set (payload instructions) -> load instructions into internal state, stop instruction execution if needed.
+* /api/play -> start instruction execution
+* /api/pause -> halts instruction execution
+* /api/stop -> resets instruction execution
+*/
+
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+	// connect to WIFI
+
+	// obtain IP address
+
+	// start Web Server
+
+	// "web server available at <ip>"
 }
