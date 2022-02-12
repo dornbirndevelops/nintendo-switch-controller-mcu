436cbd1efc5a1c59990f915bcdd9f29cc2b5ddd3
Anthony Sottile <asottile@umich.edu>
2022-02-12 17:22:52 -0500

    use white pixel detection for space-time distortions

--- std.py
+++ std.py
@@ -21,21 +21,28 @@ def _press(ser: serial.Serial, s: str, duration: float = .1) -> None:
     time.sleep(.075)
 
 
-def _getframe(vid: cv2.VideoCapture, *, guide: bool = True) -> numpy.ndarray:
+def _dim(frame: numpy.ndarray) -> tuple[int, int, int, int]:
+    px = int(len(frame[0]) * 570 / 1510)
+    py = int(len(frame) * 130 / 850)
+
+    w = int(len(frame[0]) * 100 / 1510)
+    h = int(len(frame) * 10 / 850)
+
+    return (px, py, w, h)
+
+
+def _getframe(vid: cv2.VideoCapture) -> numpy.ndarray:
     _, frame = vid.read()
 
-    if guide:
-        px_1 = int(len(frame[0]) * 480 / 1510)
-        px_2 = int(len(frame[0]) * 520 / 1510)
-        py = int(len(frame) * 135 / 850)
-        for px in (px_1, px_2):
-            cv2.rectangle(
-                frame,
-                (px - 2, py - 2),
-                (px + 2, py + 2),
-                (0, 0, 255),
-                2,
-            )
+    px, py, w, h = _dim(frame)
+
+    cv2.rectangle(
+        frame,
+        (px, py),
+        (px + w, py + h),
+        (0, 0, 255),
+        1,
+    )
 
     cv2.imshow('game', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -65,28 +72,6 @@ def _shh(ser: serial.Serial) -> Generator[None, None, None]:
         ser.write(b'.')
 
 
-def _avg_color(
-        frame: numpy.ndarray,
-        y: int,
-        x: int,
-) -> tuple[float, float, float]:
-    sums = [0] * 3
-
-    for dy in (-1, 0, 1):
-        for dx in (-1, 0, 1):
-            for i, v in enumerate(frame[y + dy][x + dx]):
-                sums[i] += v
-
-    return (sums[0] / 9, sums[1] / 9, sums[2] / 9)
-
-
-def _distance(
-        c1: tuple[float, float, float],
-        c2: tuple[float, float, float],
-) -> float:
-    return sum((b - a) ** 2 for a, b in zip(c1, c2))
-
-
 def main() -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument('--serial', default=SERIAL_DEFAULT)
@@ -96,24 +81,11 @@ def main() -> int:
     vid.set(cv2.CAP_PROP_FRAME_WIDTH, 768)
     vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-    # average color
-    # did it change significantly?
-    # last refresh time (to open and close the map)
-
     with serial.Serial(args.serial, 9600) as ser, _shh(ser):
-        t0 = time.monotonic()
-
-        frame = _getframe(vid)
-        py = int(len(frame) * 135 / 850)
-        px_1 = int(len(frame[0]) * 480 / 1510)
-        px_2 = int(len(frame[0]) * 520 / 1510)
-
-        frame = _getframe(vid, guide=False)
-        color_1 = _avg_color(frame, py, px_1)
-        color_2 = _avg_color(frame, py, px_2)
+        t0 = None
 
         while True:
-            if (time.monotonic() - t0) >= 3 * 60 + 5:
+            if t0 is None or (time.monotonic() - t0) >= 3 * 60 + 5:
                 print('-> menuing to prevent sleep')
                 _press(ser, '-')
                 _press(ser, 'B')
@@ -122,18 +94,14 @@ def main() -> int:
 
             _wait_and_render(vid, .25)
 
-            frame = _getframe(vid, guide=False)
-            new_color_1 = _avg_color(frame, py, px_1)
-            new_color_2 = _avg_color(frame, py, px_2)
+            frame = _getframe(vid)
+            px, py, w, h = _dim(frame)
 
-            distance_1 = _distance(color_1, new_color_1)
-            distance_2 = _distance(color_2, new_color_2)
-            if distance_1 >= 1800 and distance_2 >= 1800:
-                print(f'space time?! {distance_1=} {distance_2=}')
+            filtered = frame[py:py+h, px:px+w, :] > (200, 200, 200)
+            whites = numpy.apply_along_axis(all, axis=2, arr=filtered).sum()
+            if whites >= 10:
+                print(f'space time?! {whites=}')
                 _alarm(ser, vid)
-
-            color_1 = new_color_1
-            color_2 = new_color_2
 
     vid.release()
     cv2.destroyAllWindows()
